1556af2de72c69a11e28d0592bb8dd5cc6d33fe9
Oleg <ogordienov@yandex.ru>
2022-06-28 15:42:06 +0200

    fix: render button after choosen broker

--- src/components/SelectBrokerPopup/SelectBrokerPopup.js
+++ src/components/SelectBrokerPopup/SelectBrokerPopup.js
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import CloseIcon from '@mui/icons-material/Close';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import Visibility from '@mui/icons-material/Visibility';
-import './style.css';
-import { getUserBrokers, postUserBroker } from '../../api/brokers';
-import { useSelector } from 'react-redux';
-import { selectUserCredentials } from '../../slices/authSlice';
-import { patchUserBrokerById } from '../../api/users';
-import { LoadingButton } from '@mui/lab';
+import React, { useState } from 'react'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import CloseIcon from '@mui/icons-material/Close'
+import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import Visibility from '@mui/icons-material/Visibility'
+import './style.css'
+import { getUserBrokers, postUserBroker } from '../../api/brokers'
+import { useSelector } from 'react-redux'
+import { selectUserCredentials } from '../../slices/authSlice'
+import { patchUserBrokerById } from '../../api/users'
+import { LoadingButton } from '@mui/lab'
 
 const SelectBrokerPopup = ({
   open,
   handleClose,
   brokerConfig,
-  onSubmit
+  onSubmit,
 }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
   const {
     publicId,
-    authToken
-  } = useSelector(selectUserCredentials);
+    authToken,
+  } = useSelector(selectUserCredentials)
 
   const [values, setValues] = React.useState({
     amount: '',
@@ -33,7 +33,7 @@ const SelectBrokerPopup = ({
     weight: '',
     weightRange: '',
     showPassword: false,
-  });
+  })
 
   const style = {
     position: 'absolute',
@@ -44,91 +44,92 @@ const SelectBrokerPopup = ({
     height: 700,
     border: '10px solid rgb(30,30,30 , 0) !important',
     p: 4,
-  };
+  }
 
   const isOanda = brokerConfig.name?.toLowerCase()
-    .includes('oanda');
+    .includes('oanda')
 
   const patchBrokerCredentials = async (brokerId, patchData, userBrokers) => {
     try {
-      const brokerToUpdate = userBrokers.find(data => data.broker_id === brokerId);
-      console.log('brokerToUpdate: ', brokerToUpdate);
-      console.log('patchData: ', patchData);
+      const brokerToUpdate = userBrokers.find(data => data.broker_id === brokerId)
+      console.log('brokerToUpdate: ', brokerToUpdate)
+      console.log('patchData: ', patchData)
 
       for (const [key, value] of Object.entries(patchData)) {
 
-        const currentField = brokerToUpdate.user_broker_setting.find(data => data.broker_setting.option_name.toLowerCase() === key);
-        console.log('currentField ', currentField);
+        const currentField = brokerToUpdate.user_broker_setting.find(data => data.broker_setting.option_name.toLowerCase() === key)
+        console.log('currentField ', currentField)
 
         await patchUserBrokerById(publicId, authToken, {
           id: currentField.id,
           broker_setting_id: currentField.broker_setting_id,
-          option_value: value
-        });
-        console.log(`${key}: ${value}`);
+          option_value: value,
+        })
+        console.log(`${key}: ${value}`)
       }
     } catch (err) {
-      console.log('error: ', err.message);
+      console.log('error: ', err.message)
     }
-  };
+  }
 
   const handleSubmit = async (event) => {
-    setIsLoading(true);
+    setIsLoading(true)
     try {
-      event.preventDefault();
+      event.preventDefault()
 
-      const form = event.currentTarget;
-      const isValid = form.checkValidity();
+      const form = event.currentTarget
+      const isValid = form.checkValidity()
 
       if (!isValid) {
-        console.log('not valid inputs'); // todo add logic
-        return;
+        setIsLoading(false)
+        console.log('not valid inputs') // todo add logic
+        return
       }
 
-      let userData = Object.fromEntries(new FormData(form));
-      console.log('userData: ', userData, '\nbrokerConfig: ', brokerConfig);
-      const postBroker = await postUserBroker(publicId, authToken, brokerConfig.id);
+      let userData = Object.fromEntries(new FormData(form))
+      console.log('userData: ', userData, '\nbrokerConfig: ', brokerConfig)
+      const postBroker = await postUserBroker(publicId, authToken, brokerConfig.id)
 
       if (postBroker === null) {
-        console.log('ok');
+        console.log('ok')
         setValues({
           ...values,
-          password: ''
-        });
-        const userBrokers = await getUserBrokers(publicId, authToken);
-        await patchBrokerCredentials(brokerConfig.id, userData, userBrokers);
-        onSubmit();
+          password: '',
+        })
+        const userBrokers = await getUserBrokers(publicId, authToken)
+        await patchBrokerCredentials(brokerConfig.id, userData, userBrokers)
+        onSubmit()
       }
-      handleClose();
+      handleClose()
     } catch (err) {
-      console.log('error when submit: ', err.message);
+      console.log('error when submit: ', err.message)
     }
-    setIsLoading(false);
-  };
+    setIsLoading(false)
+  }
 
   const handleChange = (prop) => (event) => {
     setValues({
       ...values,
-      [prop]: event.target.value
-    });
-  };
+      [prop]: event.target.value,
+    })
+  }
 
   const handleClickShowPassword = () => {
     setValues({
       ...values,
       showPassword: !values.showPassword,
-    });
-  };
+    })
+  }
 
   const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
+    event.preventDefault()
+  }
 
   return (
     <Modal
       open={open}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
+      aria-labelledby='modal-modal-title'
+      aria-describedby='modal-modal-description'
     >
       <Box sx={style}>
         <Box
@@ -144,21 +145,21 @@ const SelectBrokerPopup = ({
         >
           <Grid
             container
-            direction="row"
-            justifyContent="flex-end"
-            alignItems="center"
+            direction='row'
+            justifyContent='flex-end'
+            alignItems='center'
           >
             <CloseIcon
               onClick={handleClose}
               sx={{
                 color: 'white',
                 margin: '15px 15px 0px 0px',
-                cursor: 'pointer'
+                cursor: 'pointer',
               }}
             />
           </Grid>
           <Box
-            component="form"
+            component='form'
             onSubmit={handleSubmit}
             noValidate
             sx={{ mt: 1 }}
@@ -171,9 +172,9 @@ const SelectBrokerPopup = ({
               </p>
               <Grid
                 container
-                direction="row"
-                justifyContent="space-between"
-                alignItems="center"
+                direction='row'
+                justifyContent='space-between'
+                alignItems='center'
               >
                 {isOanda && <><Grid>
                   <h5>Token</h5>
@@ -195,18 +196,18 @@ const SelectBrokerPopup = ({
                       style: {
                         color: 'white',
                         fontSize: 15,
-                        height: 30
+                        height: 30,
                       },
                     }}
-                    className="inputField1"
-                    margin="normal"
-                    placeholder="Enter token"
+                    className='inputField1'
+                    margin='normal'
+                    placeholder='Enter token'
                     required
                     fullWidth
-                    id="token"
-                    type="text"
-                    size="small"
-                    name="token"
+                    id='token'
+                    type='text'
+                    size='small'
+                    name='token'
                   /></>}
                 {!isOanda && <><Grid>
                   <h5>Username</h5>
@@ -228,18 +229,18 @@ const SelectBrokerPopup = ({
                       style: {
                         color: 'white',
                         fontSize: 15,
-                        height: 30
+                        height: 30,
                       },
                     }}
-                    className="inputField1"
-                    margin="normal"
-                    placeholder="Enter username"
+                    className='inputField1'
+                    margin='normal'
+                    placeholder='Enter username'
                     required
                     fullWidth
-                    id="username"
-                    type="text"
-                    size="small"
-                    name="username"
+                    id='username'
+                    type='text'
+                    size='small'
+                    name='username'
                   />
                   <Grid>
                     <h5>Password</h5>
@@ -261,34 +262,34 @@ const SelectBrokerPopup = ({
                       style: {
                         color: 'white',
                         fontSize: 15,
-                        height: 30
+                        height: 30,
                       },
                     }}
-                    className="inputField1"
-                    margin="normal"
-                    placeholder="Enter password"
+                    className='inputField1'
+                    margin='normal'
+                    placeholder='Enter password'
                     required
                     fullWidth
-                    size="small"
-                    name="password"
+                    size='small'
+                    name='password'
                     type={values.showPassword ? 'text' : 'password'}
                     value={values.password}
                     onChange={handleChange('password')}
-                    id="password"
-                    autoComplete="current-password"
+                    id='password'
+                    autoComplete='current-password'
                     InputProps={{
                       endAdornment: (
-                        <InputAdornment position="end">
+                        <InputAdornment position='end'>
                           <IconButton
-                            aria-label="toggle password visibility"
+                            aria-label='toggle password visibility'
                             onClick={handleClickShowPassword}
                             onMouseDown={handleMouseDownPassword}
-                            edge="end"
+                            edge='end'
                           >
                             {values.showPassword ? (
-                              <VisibilityOff sx={{ color: 'gray' }}/>
+                              <VisibilityOff sx={{ color: 'gray' }} />
                             ) : (
-                              <Visibility sx={{ color: 'gray' }}/>
+                              <Visibility sx={{ color: 'gray' }} />
                             )}
                           </IconButton>
                         </InputAdornment>
@@ -315,18 +316,18 @@ const SelectBrokerPopup = ({
                       style: {
                         color: 'white',
                         fontSize: 15,
-                        height: 30
+                        height: 30,
                       },
                     }}
-                    className="inputField1"
-                    margin="normal"
-                    placeholder="Enter API key"
+                    className='inputField1'
+                    margin='normal'
+                    placeholder='Enter API key'
                     required
                     fullWidth
-                    id="api_key"
-                    type="text"
-                    size="small"
-                    name="api_key"
+                    id='api_key'
+                    type='text'
+                    size='small'
+                    name='api_key'
                   /></>}
                 <Grid>
                   <h5>Default account</h5>
@@ -348,25 +349,25 @@ const SelectBrokerPopup = ({
                     style: {
                       color: 'white',
                       fontSize: 15,
-                      height: 30
+                      height: 30,
                     },
                   }}
-                  className="inputField1"
-                  margin="normal"
-                  placeholder="Enter Default account"
+                  className='inputField1'
+                  margin='normal'
+                  placeholder='Enter Default account'
                   required
                   fullWidth
-                  id="accountid"
-                  type="text"
-                  size="small"
-                  name="accountid"
+                  id='accountid'
+                  type='text'
+                  size='small'
+                  name='accountid'
                 />
               </Grid>
               <LoadingButton
-                type="submit"
+                type='submit'
                 fullWidth
                 loading={isLoading}
-                variant="text"
+                variant='text'
                 sx={{
                   mt: 3,
                   mb: 2,
@@ -374,8 +375,8 @@ const SelectBrokerPopup = ({
                   color: '#ffffff',
                   '&:hover': {
                     backgroundColor: 'primary.main',
-                    opacity: [0.9, 0.8, 0.7]
-                  }
+                    opacity: [0.9, 0.8, 0.7],
+                  },
                 }}
                 style={{
                   textDecoration: 'none',
@@ -391,7 +392,7 @@ const SelectBrokerPopup = ({
         </Box>
       </Box>
     </Modal>
-  );
-};
+  )
+}
 
-export default SelectBrokerPopup;
+export default SelectBrokerPopup
--- src/components/SelectBrokerScreen/SelectBroker.js
+++ src/components/SelectBrokerScreen/SelectBroker.js
@@ -1,98 +1,99 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import './style.css';
-import MainPricingDashboard from '../MainPricingDashboard/MainPricingDashboard';
-import RenderBrokers from '../RenderBrokers/RenderBrokers';
-import { getAllBrokers, getUserBrokers } from '../../api/brokers';
-import { useSelector } from 'react-redux';
-import { selectUserCredentials } from '../../slices/authSlice';
-import SelectBrokerPopup from '../SelectBrokerPopup/SelectBrokerPopup';
-import { getUserById, patchUserById } from '../../api/users';
-import { useNavigate } from 'react-router-dom';
+import * as React from 'react'
+import { useEffect, useState } from 'react'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import './style.css'
+import MainPricingDashboard from '../MainPricingDashboard/MainPricingDashboard'
+import RenderBrokers from '../RenderBrokers/RenderBrokers'
+import { getAllBrokers, getUserBrokers } from '../../api/brokers'
+import { useSelector } from 'react-redux'
+import { selectUserCredentials } from '../../slices/authSlice'
+import SelectBrokerPopup from '../SelectBrokerPopup/SelectBrokerPopup'
+import { getUserById, patchUserById } from '../../api/users'
+import { useNavigate } from 'react-router-dom'
 
 export default function SelectBroker() {
-  const [popupIsOpen, setPopupIsOpen] = useState(false);
-  const [brokerSelected, setBrokerSelected] = useState(false);
-  const [brokerConfig, setBrokerConfig] = useState({});
-  const [brokers, setBrokers] = useState([]);
-  const [userBrokers, setUserBrokers] = useState([]);
-  const navigate = useNavigate();
+  const [popupIsOpen, setPopupIsOpen] = useState(false)
+  const [brokerSelected, setBrokerSelected] = useState(false)
+  const [brokerConfig, setBrokerConfig] = useState({})
+  const [brokers, setBrokers] = useState([])
+  const [userBrokers, setUserBrokers] = useState([])
+  const navigate = useNavigate()
 
   const {
     publicId,
-    authToken
-  } = useSelector(selectUserCredentials);
+    authToken,
+  } = useSelector(selectUserCredentials)
 
   const handleOpen = (config) => {
-    setPopupIsOpen(true);
-    setBrokerConfig(config);
-  };
-  const handleClose = () => setPopupIsOpen(false);
+    setPopupIsOpen(true)
+    setBrokerConfig(config)
+  }
+  const handleClose = () => setPopupIsOpen(false)
 
   const handlePopupSubmit = async () => {
-    const userBrokersData = await getUserBrokers(publicId, authToken);
-    setUserBrokers(userBrokersData);
-  };
+    const userBrokersData = await getUserBrokers(publicId, authToken)
+    setUserBrokers(userBrokersData)
+    setBrokerSelected(userBrokersData.length !== 0)
+  }
 
   const handleClick = () => {
-    navigate('./');
-  };
+    navigate('../')
+  }
 
   useEffect(() => {
     (async () => {
-      const brokersData = await getAllBrokers();
-      const userBrokersData = await getUserBrokers(publicId, authToken);
-      const user = await getUserById(publicId, authToken);
+      const brokersData = await getAllBrokers()
+      const userBrokersData = await getUserBrokers(publicId, authToken)
+      const user = await getUserById(publicId, authToken)
 
-      setBrokers(brokersData);
-      setUserBrokers(userBrokersData);
-      setBrokerSelected(userBrokersData.length !== 0);
+      setBrokers(brokersData)
+      setUserBrokers(userBrokersData)
+      setBrokerSelected(userBrokersData.length !== 0)
       if (!user.risk) {
         patchUserById(publicId, authToken, { risk: 2 })
           .then(() => console.log('risk set'))
-          .catch(err => console.log('broker data not sent', err)); // todo add logic;
+          .catch(err => console.log('broker data not sent', err)) // todo add logic;
       } else {
-        console.log('risk: ', user.risk);
+        console.log('risk: ', user.risk)
       }
-    })();
-  }, []);
+    })()
+  }, [])
 
   return (
     <React.Fragment>
       <MainPricingDashboard>
         <Grid sx={{ backgroundColor: '#0f0f11' }}>
           <SelectBrokerPopup open={popupIsOpen} handleClose={handleClose}
-                             brokerConfig={brokerConfig} onSubmit={handlePopupSubmit}/>
+                             brokerConfig={brokerConfig} onSubmit={handlePopupSubmit} />
           <Container
-            maxWidth="lg"
-            component="main"
+            maxWidth='lg'
+            component='main'
             sx={{
               backgroundColor: 'none',
-              height: '100vh'
+              height: '100vh',
             }}
           >
             <Grid
               container
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
+              direction='row'
+              justifyContent='center'
+              alignItems='center'
             >
               <h1>Select your broker</h1>
             </Grid>
 
             <Grid
               container
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
+              direction='row'
+              justifyContent='center'
+              alignItems='center'
             >
               <p>Your membership starts as soon as you set up payment. </p>
             </Grid>
-            <Container component="main" maxWidth="sm">
+            <Container component='main' maxWidth='sm'>
               <Grid sx={{ backgroundColor: '#0f0f11' }}>
                 <Box
                   px={3}
@@ -108,12 +109,12 @@ export default function SelectBroker() {
                 >
                   <Grid
                     container
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="center"
+                    direction='row'
+                    justifyContent='space-between'
+                    alignItems='center'
                   >
                     <RenderBrokers handleOpen={handleOpen} brokers={brokers}
-                                   userBrokers={userBrokers}/>
+                                   userBrokers={userBrokers} />
                   </Grid>
                 </Box>
                 <Box
@@ -130,9 +131,9 @@ export default function SelectBroker() {
                 >
                   <Grid
                     container
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="center"
+                    direction='row'
+                    justifyContent='space-between'
+                    alignItems='center'
                   >
                     <Grid>
                       <p style={{ margin: 0 }}>How does this work?</p>
@@ -152,10 +153,10 @@ export default function SelectBroker() {
                 </Box>
                 {brokerSelected ? (
                   <Button
-                    type="button"
+                    type='button'
                     onClick={handleClick}
                     fullWidth
-                    variant="contained"
+                    variant='contained'
                     sx={{
                       mt: 3,
                       mb: 2,
@@ -178,5 +179,5 @@ export default function SelectBroker() {
         </Grid>
       </MainPricingDashboard>
     </React.Fragment>
-  );
+  )
 }
